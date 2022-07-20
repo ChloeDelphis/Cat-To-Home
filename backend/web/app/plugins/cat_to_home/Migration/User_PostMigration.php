4a282efbaa90a8fe66d9e2d1a33928d1608b1631
Steph934 <quinionstephane@gmail.com>
2022-07-20 12:25:30 +0200

    add autoload, custom table, custom endpoint

---
+++ backend/web/app/plugins/cat_to_home/Migration/User_PostMigration.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Migration;
+
+class User_PostMigration
+{
+    const TABLE_NAME = "wp_cat_to_home_user_post";
+    public static function createTable()
+    {
+        // Récupération de la variable $wpdb
+        global $wpdb;
+
+        // On défini la collation
+        $charset = $wpdb->get_charset_collate();
+
+        // Création de la requête
+        $sql = "CREATE TABLE " . self::TABLE_NAME . "(
+            id_adoption mediumint(9) NOT NULL,
+            id_adopter mediumint(9) NOT NULL,
+            PRIMARY KEY (id_adption, id_adopter)
+        ) $charset;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
+
+    public static function deleteTable()
+    {
+        global $wpdb;
+        $wpdb->query("DROP TABLE IF EXISTS" . self::TABLE_NAME . "");
+    }
+}
--- backend/web/app/plugins/cat_to_home/cat_to_home.php
+++ backend/web/app/plugins/cat_to_home/cat_to_home.php
@@ -6,6 +6,10 @@ Author: Team Cat to home
 Version: 1.0
 */
 
+require __DIR__ . '/vendor/autoload.php';
+
+use Migration\User_PostMigration;
+
 // Customs post-types
 require_once plugin_dir_path(__FILE__) . './post-types/cat.php';
 
@@ -16,3 +20,19 @@ require_once plugin_dir_path(__FILE__) . './taxonomies/location.php';
 require_once plugin_dir_path(__FILE__) . './taxonomies/sex.php';
 require_once plugin_dir_path(__FILE__) . './taxonomies/vaccinate.php';
 
+// Custom endpoint
+require_once plugin_dir_path(__FILE__) . './custom-endpoints/registration.php';
+
+register_activation_hook(__FILE__, 'cat_to_home_create_custom_roles');
+function cat_to_home_create_custom_roles()
+{
+    // cat_to_home_create_cook_role();
+    User_PostMigration::createTable();
+}
+
+register_deactivation_hook(__FILE__, 'cat_to_home_remove_custom_roles');
+function cat_to_home_remove_custom_roles()
+{
+    // cat_to_home_remove_cook_role();
+    User_PostMigration::deleteTable();
+}
