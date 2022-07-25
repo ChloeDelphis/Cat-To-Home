6155179f9491c2d30b1bae628a72980c32d16ad8
Damien-LAITANI <d.laitani@gmail.com>
2022-07-25 19:10:45 +0200

    feature 'cat location'

---
+++ frontend/src/components/home/ItemListLocation.vue
@@ -0,0 +1,34 @@
+<template>
+  <p @click="choiceLocation" class="input">{{code}} - {{name}}</p>
+</template>
+
+<script>
+export default {
+    name: 'ItemListLocation',
+    props: {
+        code: String,
+        name: String
+    },
+    methods: {
+        choiceLocation(event) {
+            this.$emit('choiceLocation', event);
+        }
+    }
+
+}
+</script>
+
+<style>
+
+.home__form__list .input {
+    
+    position: relative;
+    bottom: 2px;
+    border-bottom: 1px solid #E3B8AA;
+    cursor: pointer;
+}
+
+.home__form__list .input:hover {
+    opacity: 0.7;
+}
+</style>
