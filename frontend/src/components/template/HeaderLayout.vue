b21f117714b62263a2324d9bffd42ddb2162e0ad
Steph934 <quinionstephane@gmail.com>
2022-07-25 09:28:31 +0200

    vue- add link Connexion & Registration

--- frontend/src/components/template/HeaderLayout.vue
+++ frontend/src/components/template/HeaderLayout.vue
@@ -1,45 +1,55 @@
 <template>
-      <header class="header">
+  <header class="header">
+    <h1>
+      <router-link :to="{ name: 'home' }"
+        ><img
+          class="header__logo"
+          src="../../assets/logo/Logo.png"
+          alt="logoCatToHome"
+        />
+      </router-link>
+    </h1>
+    <nav class="header__nav">
+      <div class="header__nav--mobtab">
+        <a href="#">|||</a>
+      </div>
 
-        <h1>
-            <router-link :to="{name: 'home'}"><img class="header__logo" src="../../assets/logo/Logo.png" alt="logoCatToHome">
-            </router-link>
-        </h1>
-        <nav class="header__nav">
-
-            <div class="header__nav--mobtab">
-                <a href="#">|||</a>
-            </div>
-
-            <!-- pour faire apparaître le menu en mode mobile et tablette :
+      <!-- pour faire apparaître le menu en mode mobile et tablette :
                 Ajouter la classe header__nav--small à la div ci-dessous
             Ajouter la classe fixed à l'élément header  -->
 
-            <div class="header__nav--desktop">
-                <router-link class="header__nav__don button__orange--papate" :to="{name: 'cat_add'}">
-                    Je donne un chat</router-link>
-                <a class="header__nav__menu" href="#">À propos</a>
-                <a class="header__nav__menu" href="#">Les chats</a>
-                <!-- <a class="header__nav__menu" href="#">Inscription</a>
-                <a class="header__nav__menu" href="#">Connexion</a>
-                <a class="header__nav__menu" href="#">Déconnexion</a> -->
+      <div class="header__nav--desktop">
+        <router-link
+          class="header__nav__don button__orange--papate"
+          :to="{ name: 'cat_add' }"
+        >
+          Je donne un chat</router-link
+        >
+        <a class="header__nav__menu" href="#">À propos</a>
+        <a class="header__nav__menu" href="#">Les chats</a>
+        <a class="header__nav__menu" href="../../views/RegistrationView.vue"
+          >Inscription</a
+        >
+        <a class="header__nav__menu" href="../../views/ConnexionView.vue"
+          >Connexion</a
+        >
+        <!-- <a class="header__nav__menu" href="#">Déconnexion</a>  -->
 
-                <a class="header__nav__menu" href="#">Contact</a>
-                <a class="header__nav__profile" href="#"><img class="header__nav__profile--icon"
-                        src="../../assets/icones/profil.png" alt="icône vers profil utilisateur"><span
-                        class="header__nav__profile--text">Profil</span></a>
-            </div>
-
-        </nav>
-
-    </header>
-
+        <a class="header__nav__menu" href="#">Contact</a>
+        <a class="header__nav__profile" href="#"
+          ><img
+            class="header__nav__profile--icon"
+            src="../../assets/icones/profil.png"
+            alt="icône vers profil utilisateur"
+          /><span class="header__nav__profile--text">Profil</span></a
+        >
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss">
