b4496329e345e65e34ed3849bb220811f91f01e4
AntoineGuillo55 <guillo.antoine93@gmail.com>
2022-07-25 10:32:30 +0200

    Création composant CatLayout et view pour le détail d'une fiche, avec route et lien sur les cards pour afficher le détail

--- frontend/src/components/cat/CatCardLayout.vue
+++ frontend/src/components/cat/CatCardLayout.vue
@@ -1,12 +1,14 @@
 <template>
-  <article class="post">
-      <div class="post__img"></div>
-      <div class="post__content">
-          <span class="post__cat__name">Timothy</span>
-          <span class="post__cat__age">8 month</span>
-          <p class="post__cat__localisation"><span class="bold">Localisation</span>- RENNES FRANCE</p>
-      </div>  
-  </article>
+    <router-link :to="{name: 'cat'}" >
+      <article class="post">
+          <div class="post__img"></div>
+          <div class="post__content">
+              <span class="post__cat__name">Timothy</span>
+              <span class="post__cat__age">8 month</span>
+              <p class="post__cat__localisation"><span class="bold">Localisation</span>- RENNES FRANCE</p>
+          </div>  
+      </article>
+    </router-link>
 </template>
 
 <script>
