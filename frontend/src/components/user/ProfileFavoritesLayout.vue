23f0f046f15b9b4591ca0fd257e27fc7a0856bd7
ChloeDelphis <chloe.delphis@gmail.com>
2022-07-25 19:28:31 +0200

    Profile calls every component needed

--- frontend/src/components/user/ProfileFavoritesLayout.vue
+++ frontend/src/components/user/ProfileFavoritesLayout.vue
@@ -3,10 +3,6 @@
     <h2>Mes coups de coeur</h2>
     <div class="post__list__container">
       <CatCardLayout />
-      <CatCardLayout />
-      <CatCardLayout />
-      <CatCardLayout />
-      <CatCardLayout />
     </div>
 
     <!-- Boutons présents dans la maquette et l'inté mais peut-être à ne faire apparaître que si le nombre de résultats est supérieur à 9<div class="post__list__navigation">
