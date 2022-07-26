f563a5b35481c385dda07137aabcb79d130aa248
ChloeDelphis <chloe.delphis@gmail.com>
2022-07-26 13:15:12 +0200

    error fields placement and customization, most checks done

---
+++ frontend/src/services/user/UserService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://cat_to_home.local/wp-json',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        // Cette syntaxe permet d'effectuer une requete qui necessite d'etre connecté
+        //Authorization: 'Bearer ' + sessionStorage.getItem('token') + ''
+    },
+    timeout: 10000
+});
+
+export default {
+    // permet de créer un nouvel utlisateur
+    async register(params) {
+        try {
+            const response = await apiClient.post('/wp/v2/users/register', params);
+            return response.data
+        } catch(error) {
+            return error.response.data
+        }
+    },
+}
