22b3a177fbd3f97000a3b20175f3931380685644
P1errO <bourriket7@gmail.com>
2022-07-26 10:40:53 +0200

    modif

--- frontend/src/services/user/UserService.js
+++ frontend/src/services/user/UserService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://cat-to-home.local/wp-json',
+    baseURL: 'http://cat_to_home.local/wp-json',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
