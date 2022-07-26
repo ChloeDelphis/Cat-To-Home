8001695ec04b00fa891751d50e99fbfcdf668f6e
P1errO <bourriket7@gmail.com>
2022-07-26 10:37:09 +0200

    fonctionnaliter de connection et deconnection

--- frontend/src/components/user/LoginFormLayout.vue
+++ frontend/src/components/user/LoginFormLayout.vue
@@ -4,21 +4,27 @@
       <img class="connexion__img" src="../../assets/img/illu_cat_door.png"
         alt="chat devant un portail de voyage spatio-temporel" />
 
-      <div class="connexion__form">
+      <div v-on:keyup.enter="login" class="connexion__form">
         <h2 class="connexion__form__title">Connexion</h2>
 
         <fieldset class="connexion__form__fieldset">
           <div class="connexion__form__fieldset__field">
             <label for="email">Adresse e-mail</label><br />
-            <input class="connexion__form__fieldset__field__input input" type="text" />
+            <input v-model="email" class="connexion__form__fieldset__field__input input" type="text" />
           </div>
           <div class="connexion__form__fieldset__field">
             <label for="password">Mot de passe</label><br />
-            <input class="connexion__form__fieldset__field__input input" type="password" />
+            <input v-model="password" class="connexion__form__fieldset__field__input input" type="password" />
           </div>
+          <ul class="field__error-list">
+              <li>
+                {{ passwordError }}
+                {{ emailError }}
+              </li>
+          </ul>
         </fieldset>
 
-        <button class="connexion__form__button button__orange--papate">
+        <button v-on:click="login" class="connexion__form__button button__orange--papate">
           Je me connecte
         </button>
       </div>
@@ -27,7 +33,49 @@
 </template>
 
 <script>
-export default {};
+import UserService from '@/services/user/UserService';
+
+export default {
+  name: 'LoginFormLayout',
+  data() {
+        return {
+            passwordError: null,
+            emailError: null,
+            email: null,
+            password: null
+        }
+    },
+    methods: {
+        async login() {
+            if(!this.email) {
+                this.emailError = "Email / email cannot be empty";
+            }
+
+            if(!this.password) {
+                this.passwordError = "Password cannot be empty";
+            }
+
+            if(!this.passwordError && !this.emailError) {
+                console.log('LOGIN');
+                // Requete Ajax pour connexion utilisateur
+                const response = await UserService.login({
+                  username: this.email,
+                  password: this.password
+                })
+                if(response.success === true) {
+                  console.log('OK');
+                  // On execute une mutation pour stocker le token dans le sessionStorage
+                  // Et le synchroniser avec le store afin de rendre notre store.token reactif
+                  this.$store.commit('setToken', response.data.token);
+                  // On fait une redirection
+                  this.$router.push({name: 'home'});
+                } else {
+                  alert(response.message)
+                }
+            }
+        }
+    }
+};
 </script>
 
 <style lang="scss">
