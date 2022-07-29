59155bdb2d581eefaa4502b7b34fb57259c3c282
ChloeDelphis <chloe.delphis@gmail.com>
2022-07-29 10:32:13 +0200

    commit to switch to develop and get png

--- frontend/src/components/cat/CatCardLayout.vue
+++ frontend/src/components/cat/CatCardLayout.vue
@@ -3,7 +3,9 @@
       <article class="post">
           <!--Image à ajouter en v-if si le composant est appelé par une page spécifique (profil adoptant) <img class="hearth" src="../../assets/icones/coeur.png" alt="" /> -->
 
-          <div class="post__img" v-bind:style="'background-image:url(' + picture + ')'"></div>
+          <div class="post__img" v-bind:style="'background-image:url(' + picture + ')'">
+            <img src="../../assets/icones/coeur.png" alt="">
+          </div>
           <div class="post__content">
               <span class="post__cat__name" v-html="name"></span>
               <span class="post__cat__age">8 month</span>
