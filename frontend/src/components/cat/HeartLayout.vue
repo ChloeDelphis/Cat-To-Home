0ca8b040982460f2d08cd42b98535d06d203613a
ChloeDelphis <chloe.delphis@gmail.com>
2022-08-03 17:19:09 +0200

    display messages on every component where heartlayout is called

--- frontend/src/components/cat/HeartLayout.vue
+++ frontend/src/components/cat/HeartLayout.vue
@@ -38,6 +38,15 @@ export default {
       // On empêche d'aller sur la page détail
       event.preventDefault();
 
+      // On sélectionne l'élément message qui est lié au clic
+      // (utile quand la fonction est appelée depuis la liste des chats et où il y a autant d'éléments message que de chartes)
+      let currentT = event.currentTarget;
+      console.log("current target " + currentT);
+      let parentElmt = currentT.parentNode;
+      console.log("parent target " + parentElmt);
+      let msgElmt = parentElmt.querySelector("#message");
+      console.log(msgElmt);
+
       // Si le chat fait partie des favoris
       if (this.favorite) {
         console.log("le chat " + this.name + " fait partie des favoris ");
@@ -46,21 +55,25 @@ export default {
           cat_id: this.id,
         });
         if (response.code === 200) {
-          console.log("le chat " + this.name + " a bien été retiré des favoris");
-          // On fait apparaître le message sur l'interface utilisateur
-          this.displayMessage(event);
-          // // On le fait disparaître au bout de 2 secondes
-          // setTimeout(this.displayMessage, 2000);
-          // const msgEmlt = event.currentTarget.querySelector("#message");
-          // msgEmlt.innerHTML = this.name + " a été retiré de vos favoris";
+          console.log(
+            "le chat " + this.name + " a bien été retiré des favoris"
+          );
           this.$emit("update");
+          // On fait apparaître le message sur l'interface utilisateur
+          msgElmt.classList.toggle("hidden");
+          console.log(msgElmt);
+          msgElmt.textContent = this.name + " a été retiré de vos favoris";
+          // On le fait disparaître au bout de 2 secondes
+          setTimeout(() => { msgElmt.classList.toggle("hidden") }, 2000);
         } else {
           console.log("le chat n'a pas pu être retiré des favoris");
         }
 
         // Si le chat ne fait pas partie des favoris
       } else {
-        console.log("le chat " + this.name + " ne fait pas partie des favoris ");
+        console.log(
+          "le chat " + this.name + " ne fait pas partie des favoris "
+        );
         const response = await FavoriteService.addToFavorites({
           cat_id: this.id,
         });
@@ -69,30 +82,32 @@ export default {
           console.log(
             "le chat " + this.name + " a bien été ajouté aux favoris"
           );
-          // On fait apparaître le message sur l'interface utilisateur
-          this.displayMessage(event);
-          // // On le fait disparaître au bout de 2 secondes
-          // setTimeout(this.displayMessage, 2000);
-          // const msgEmlt = event.currentTarget.querySelector("#message");
-          // msgEmlt.innerHTML = this.name + " a bien été ajouté à vos favoris";
-
           this.$emit("update");
+          // On fait apparaître le message sur l'interface utilisateur
+          msgElmt.classList.toggle("hidden")
+          msgElmt.textContent = this.name + " a bien été ajouté à vos favoris";
+          // On le fait disparaître au bout de 2 secondes
+          setTimeout(() => { msgElmt.classList.toggle("hidden") }, 2000);
         } else {
           console.log("le chat n'a pas pu être ajouté aux favoris");
         }
       }
     },
 
-    displayMessage() {
-
-      const msgEmlt = document.querySelector("#message");
-      msgEmlt.classList.toggle("hidden");
-      setTimeout(document.querySelector("#message").classList.toggle("hidden"), 2000);
-    },
+    // displayMessage(event) {
+    //   // On sélectionne le bon élément message
+    //   let currentTarget = event.currentTarget;
+    //   console.log("current target " + currentTarget);
+    //   let parentElmt = currentTarget.parentNode
+    //   console.log("parent target " + parentElmt);
+    //   let msgElmt = parentElmt.querySelector("#message");
+    //   console.log(msgElmt);
+    //   // On lui ajoute / retire la classe hidden
+    //   msgElmt.classList.toggle("hidden");
+    // },
   },
 };
 </script>
 
 <style scoped lang="scss" >
-
 </style>
