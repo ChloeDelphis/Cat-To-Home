b70304abf6a8003b874e16e940781f187b2a07a2
Damien-LAITANI <d.laitani@gmail.com>
2022-08-03 09:37:16 +0200

    add capability 'list_users' to owner role

--- backend/web/app/plugins/cat_to_home/Roles/Owner.php
+++ backend/web/app/plugins/cat_to_home/Roles/Owner.php
@@ -9,6 +9,7 @@ class Owner
         $capabilitie = get_role('editor',)->capabilities;
         $owner = add_role('owner', 'Owner', $capabilitie);
         $owner->add_cap('edit_users');
+        $owner->add_cap('list_users');
     }
 
     public static function cat_to_home_remove_owner_role()
