39fd8eb4831b86b4aa4757b3b92f7c067477fe52
Damien-LAITANI <d.laitani@gmail.com>
2022-08-05 09:55:33 +0200

    fix favorite token + opti errors message login page

--- frontend/src/components/user/LoginFormLayout.vue
+++ frontend/src/components/user/LoginFormLayout.vue
@@ -12,10 +12,12 @@
           <div class="connexion__form__fieldset__field">
             <label for="email">Adresse e-mail</label><br />
             <input v-model="email" class="connexion__form__fieldset__field__input input" type="text" />
+            <p class="inscription__form__fieldset__field__error">{{ loginError.email }}</p>
           </div>
           <div class="connexion__form__fieldset__field">
             <label for="password">Mot de passe</label><br />
             <input v-model="password" class="connexion__form__fieldset__field__input input" type="password" />
+            <p class="inscription__form__fieldset__field__error">{{ loginError.password }}</p>
           </div>
           <div class="center__text">
             <router-link :to="{ name: 'registration' }" class="btn--reinit__pass">Inscription</router-link> 
@@ -30,17 +32,12 @@
           <div class="send__fail">
             <p class="reinit__pass reinit__pass--hidden"></p>
           </div>
-          <ul class="field__error-list">
-            <li>
-              {{ passwordError }}
-              {{ emailError }}
-            </li>
-          </ul>
         </fieldset>
 
         <button v-on:click="login" class="connexion__form__button button__orange--papate">
           Je me connecte
         </button>
+        <p class="inscription__form__fieldset__field__error">{{ loginError.login }}</p>
       </div>
     </section>
   </main>
@@ -53,8 +50,7 @@ export default {
   name: "LoginFormLayout",
   data() {
     return {
-      passwordError: null,
-      emailError: null,
+      loginError: {},
       email: null,
       password: null,
       emailReinitPass: null,
@@ -70,18 +66,16 @@ export default {
   },
   methods: {
     async login() {
-      this.passwordError = '';
-      this.emailError = '';
+      this.loginError = {};
       if (!this.email) {
-        this.emailError = "Email cannot be empty";
+        this.loginError = {...this.loginError, email: "L'email ne doit pas être vide"};
       }
 
       if (!this.password) {
-        this.passwordError = "Password cannot be empty";
+        this.loginError = {...this.loginError, password: "Le mot de passe ne doit pas être vide"};
       }
 
-      if (!this.passwordError && !this.emailError) {
-        console.log("LOGIN");
+      if (Object.keys(this.loginError).length === 0) {
         // Requete Ajax pour connexion utilisateur
         const response = await UserService.login({
           username: this.email,
@@ -97,7 +91,7 @@ export default {
           // On fait une redirection
           this.$router.push({ name: "home" });
         } else {
-          alert(response.message);
+          this.loginError = {...this.loginError, login: "L'email ou le mot de passe ne correspond pas"}
         }
       }
     },
--- frontend/src/services/favorite/FavoriteService.js
+++ frontend/src/services/favorite/FavoriteService.js
@@ -4,9 +4,7 @@ const apiClient = axios.create({
     baseURL: 'http://cat_to_home.local/wp-json/wp/v2',
     headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json',
-        // Cette syntaxe permet d'effectuer une requete qui necessite d'être connecté
-        Authorization: 'Bearer ' + sessionStorage.getItem('token') + ''
+        'Content-Type': 'application/json'
     },
     timeout: 10000
 });
@@ -15,7 +13,6 @@ export default {
     // permet de récupérer les chats favoris d'un utilisateur donné en utilisant son bearer token
     async findAll() {
         try {
-            apiClient.defaults.headers.common['Authorization'] = '';
             const response = await apiClient.get("/users/favorites");
             return response.data;
         } catch (error) {
@@ -26,7 +23,6 @@ export default {
     // Permet d'ajouter un chat aux favoris
     async addToFavorites(params) {
         try {
-            apiClient.defaults.headers.common['Authorization'] = '';
             const response = await apiClient.post('/users/favorites/add', params);
             return response.data
         } catch (error) {
@@ -38,7 +34,6 @@ export default {
     // Permet de retirer un chat des favoris
     async removeFromFavorites(data) {
         try {
-            apiClient.defaults.headers.common['Authorization'] = '';
             // delete requests with a body need it to be set under a data key (and not params)
             const response = await apiClient.delete('/users/favorites/delete', {data});
             return response.data
